a75be199a11097a805396df7005fb8d5561454f0
ml029 <marcus.luther@rbb-online.de>
2020-02-29 22:08:06 +0100

    change name

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TimeComponent } from './component/time/time.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TimeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
